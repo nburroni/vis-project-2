7db00256362e4b244fd3d6ed1fb27e762a033849
federuiz <ruizfederico95@gmail.com>
2016-10-29 11:26:36 -0500

    100 top airports

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,7 +52,56 @@
                     .attr("cy", d => d[1])
                     .attr("r", "2px")
                     .attr("fill", "#00deff")
-                    .on("mouseover", function (d){});
+                    .on("mouseover", function (d){
+                        var labels = ['CarrierDelay','WeatherDelay','NASDelay','SecurityDelay','LateAircraftDelay'];
+                        var dataset = [1,2,3,4,5,6];
+                        var PI = Math.PI;
+                        var arcMin = 75;        // inner radius of the first arc
+                        var arcWidth = 15;      // width
+                        var arcPad = 1;         // padding between arcs
+
+                        // arc accessor
+                        //  d and i are automatically passed to accessor functions,
+                        //  with d being the data and i the index of the data
+                        var drawArc = d3.arc()
+                            .innerRadius(function(d, i) {
+                                return  arcMin;
+                            })
+                            .outerRadius(function(d, i) {
+                                return arcMin + (arcWidth);
+                            })
+                            .startAngle(0 * (PI/180))
+                            .endAngle(function(d, i) {
+                                return i * 30 * (PI/180);
+                            });
+
+                        // bind the data
+                        var arcs = svg.selectAll("path.arc-path").data(dataset);
+
+                        // *** update existing arcs -- redraw them ***
+                        arcs.attr("d", drawArc)
+                            .attr("fill", function(d){
+                                // we need to redefine the fills as well since we have new data,
+                                //  otherwise the colors would no longer be relative to the data
+                                //  values (and arc length). if your fills weren't relative to
+                                //  the data, this would not be necessary
+                                var grn = Math.floor((1 - d/60)*255);
+                                return "rgb(0, "+ grn +", 0)";
+                            });
+
+                        // draw arcs for new data
+                        arcs.enter().append("svg:path")
+                            .attr("class", "arc-path")                  // assigns a class for easier selecting
+                            .attr("transform", "translate(400,200)")    // sets position--easier than setting x's and y's
+                            .attr("fill", function(d){
+                                // fill is an rgb value with the green value determined by the data
+                                // smaller numbers result in a higher green value (1 - d/60)
+                                // you should also look into using d3 scales to create gradients
+                                var grn = Math.floor((1 - d/60)*255);
+                                return "rgb(0, "+ grn +", 0)";
+                            })
+                            .attr("d", drawArc);      // draw the arc
+                    });
 
                 d3.csv("/data/2008-compressed.csv", function (error, flights) {
                     var nodeData = d3.map(airports.map(d => {
@@ -80,7 +129,9 @@
                     }));
 
                     results.forEach((edge_subpoint_data, index, array) => {
-                        var delay = flights[index].stdDelay;
+                        var flight = flights[index];
+                        var delay = flight.stdDelay;
+
                         // for each of the arrays in the results
                         // draw a line between the subdivions points for that edge
                         var path = svg.append("path")
