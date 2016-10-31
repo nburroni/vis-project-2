ae89cfc51e6154149bb7e14a221add2bbfdc8daf
Nicolas Burroni <nmburroni@gmail.com>
2016-10-30 23:32:02 -0500

    Typo

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@
     window.visualizations = [];
 
     window.percentageToHsl = percentageToHsl;
-    window.defaultHslScale = (percentaje) => percentageToHsl(percentaje, 120, 0);
+    window.defaultHslScale = (percentage) => percentageToHsl(percentage, 120, 0);
 
     function percentageToHsl(percentage, hue0, hue1) {
         var hue = (percentage * (hue1 - hue0)) + hue0;
