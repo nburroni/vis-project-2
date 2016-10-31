9beda84f9875759fcdf85b9a2fcfb189e955e772
federuiz <ruizfederico95@gmail.com>
2016-10-30 23:32:02 -0500

    Color scale to window

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,12 @@
 
     window.visualizations = [];
 
+    window.percentageToHsl = percentageToHsl;
+    window.defaultHslScale = (percentaje) => percentageToHsl(percentaje, 120, 0);
+
+    function percentageToHsl(percentage, hue0, hue1) {
+        var hue = (percentage * (hue1 - hue0)) + hue0;
+        return 'hsl(' + hue + ', 100%, 50%)';
+    }
+    
 })();
