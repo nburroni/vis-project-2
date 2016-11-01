764b98ae885b4ab22cce76da617af51edffabfe5
federuiz <ruizfederico95@gmail.com>
2016-11-01 03:55:34 -0500

    weather csv

--- assets/js/weather-map.js
+++ assets/js/weather-map.js
@@ -25,6 +25,7 @@
         }
         setData(airports, flights, nodeData, edgeData, routeAverages, averageEdgeDataR, averageEdgeDataFN, projection, weatherEvents) {
             console.log ("started set data");
+
             d3.selectAll("#weather-map").remove();
             svg = d3.select("#map").append("svg")
                 .attr("id", "weather-map")
@@ -37,26 +38,116 @@
                 //.on("click", reset);
             g = svg.append("g")
                 .style("stroke-width", "0.5px");
-            d3.json("/data/county.json", function (error, us) {
-                g.selectAll("path")
-                    .data(topojson.feature(us, us.objects.county).features)
-                    .enter().append("path")
-                    .attr("d", path)
-                    .attr("class", "feature");
-                g.append("path")
-                    .datum(topojson.mesh(us, us.objects.county, (a, b) => a !== b))
-                    .attr("class", "mesh")
-                    .attr("d", path);
-                
-                g.selectAll('path')
-                    .filter(function(d) {
-                        return d.id == "02013"
-                    })
-                    .style('fill', 'red')
+            d3.json("/data/state.json", function (error, us) {
+                d3.json("/data/state-codes.json", function (error, stateCodes){
+                    var stateCodesList = [];
+                    for (var key in stateCodes){
+                       stateCodesList.push({name: stateCodes[key].name, code: stateCodes[key].stateAbbr});
+                    }
+                    var options = ["Hail"];
+                    var events = filterEvents(weatherEvents, options);
+                    var states = [];
+
+                    filterStates(events, states, stateCodesList);
+                    calculateStateDelays(flights, airports, states);
+                    draw(events, states, us);
+                });
             });
+
         }
 
     }
     window.visualizations.push(new WeatherMap());
 
+    function draw(events, states, us){
+        var maxDelay = Math.max.apply(Math, states.map(function(d){
+            return d.delay;
+        }));
+
+        var projection = d3.geoAlbers();
+        projection.scale(990);
+        var coordinates = events.map(d => projection([parseFloat(d.lon), parseFloat(d.lat)])).filter(d => d != null && !(isNaN(d[0]) || isNaN(d[0])));
+
+        g.selectAll("path")
+            .data(topojson.feature(us, us.objects.state).features)
+            .enter().append("path")
+            .attr("d", path)
+            .attr("class", "feature");
+        g.append("path")
+            .datum(topojson.mesh(us, us.objects.state, (a, b) => a !== b))
+            .attr("class", "mesh")
+            .attr("d", path);
+
+        g.selectAll('path')
+            .filter(function(d) {
+                if (!d.properties) console.log (d);
+                else {
+                    return findWithAttr(events, "STATE", d.properties.NAME10) > -1;
+                }
+            })
+            .style('fill', function (d){
+                if (states[findWithAttr(states, "name", d.properties.NAME10)]) return percentageToHsl((states[findWithAttr(states, "name", d.properties.NAME10)].delay / maxDelay), 120, 0);
+            });
+        g.selectAll("circle")
+            .data(coordinates).enter()
+            .append("circle")
+            .attr("cx", d => d[0])
+            .attr("cy", d => d[1])
+            .attr("r", "4px")
+            .attr("fill", "#00deff");
+    }
+    function findWithAttr(array, attr, value) {
+        for(var i = 0; i < array.length; i += 1) {
+            if(array[i][attr].toLowerCase() === value.toLowerCase()) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    function filterEvents (weatherEvents, options){
+        return weatherEvents.filter(function (d){
+            var found = false;
+            options.forEach(function (option){
+                if (option == d.EVENT_TYPE) found = true;
+            });
+            return found;
+        });
+    }
+    function filterStates(weatherEvents, states, stateCodesList){
+        weatherEvents.forEach(function (d){
+            var index = findWithAttr(states, "name", d.STATE);
+            if (index == -1){
+                states.push({name: d.STATE, code: stateCodesList[findWithAttr(stateCodesList, "name", d.STATE)].code, count: 0, delay: 0});
+            }
+        });
+    }
+    function calculateStateDelays(flights, airports, states){
+        flights.forEach(function (d){
+            var flight = d;
+            for (var j in flight){
+                if (j != "Origin" && j != "Dest" && j != "TailNum") flight[j] = parseInt(flight[j]);
+            }
+            var airport = airports[findWithAttr(airports, "iata", flight.Origin)];
+            if (airport){
+                if (!airport.flightCount) {
+                    airport.flightCount = 0;
+                    airport.WeatherDelay = 0;
+                }
+                airport.flightCount++;
+                airport.WeatherDelay += (flight.WeatherDelay);
+            }
+        });
+        airports.forEach(function (d){
+            var index = findWithAttr(states, "code", d.state);
+            if(index > -1){
+                d.WeatherDelay /= d.flightCount;
+                states[index].delay += d.WeatherDelay;
+                states[index].count++;
+            }
+        });
+        states.forEach(function (d, i){
+            if (d.count != 0) d.delay /= d.count;
+            else states.splice(i,1);
+        });
+    }
 })();
